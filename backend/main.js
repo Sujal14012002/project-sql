21c809f3b3a3725dc7e74e09ecd27ef16be3bed3
Sujal Prajapati <prajapatisujal75@gmail.com>
2025-04-17 12:11:01 +0530

    changes in form

---
+++ backend/main.js
@@ -0,0 +1,129 @@
+// const express = require("express");
+// const mysql = require("mysql2");
+// const app = express();
+// const cors = require('cors');
+// app.use(cors()); 
+
+
+
+// app.use(express.json());
+
+
+// const connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: 'Sujal@14012002',
+//     database: 'coding_school'
+// });
+
+
+// connection.connect((err) => {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log("Connected to the database");
+//     }
+// });
+
+
+
+
+
+// app.post('/post', (req, res) => {
+//     console.log("Received body:", req.body); 
+    
+
+//     const { name, email, mobile,zipcode,Adress,password,city,state,counrty } = req.body;
+    
+
+//     connection.query(
+
+
+//         'INSERT INTO schoolingdata (name, email, mobile,zipcode,Adress,password,city,state,counrty) VALUES (?, ?, ? , ?, ?, ?, ?, ?, ?)',
+//         [name, email, mobile,zipcode,Adress,password,city,state,counrty],
+
+//         (err, result) => {
+//             if (err) {
+//                 console.log(err);
+//                 return res.status(500).send('Error inserting data');
+//             }
+//             res.send("Data POSTED successfully");
+
+            
+//         }
+//     );
+// });
+
+
+
+// app.listen(3000, (err) => {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log("Server running on port 3000");
+//     }
+// });
+
+
+
+
+
+const express = require("express");
+const mysql = require("mysql2");
+const cors = require("cors");
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// DB connection
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "Sujal@14012002",
+  database: "coding_school",
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error(" Database connection failed:", err);
+  } else {
+    console.log(" Connected to MySQL database");
+  }
+});
+
+// Route using stored procedure
+app.post("/post", (req, res) => {
+  const {
+    name,
+    email,
+    mobile,
+    zipcode,
+    Adress,
+    password,
+    city,
+    state,
+    counrty,
+  } = req.body;
+
+  const query = "CALL InsertStudentData(?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+  connection.query(
+    query,
+    [name, email, mobile, zipcode, Adress, password, city, state, counrty],
+    (err, result) => {
+      if (err) {
+        console.error(" Error inserting data via SP:", err);
+        return res.status(500).send("Error inserting data via stored procedure");
+      }
+      res.send(" Data inserted via stored procedure!");
+    }
+  );
+});
+
+// Server listener
+app.listen(3000, () => {
+  console.log(" Server running on http://localhost:3000");
+});
---
+++ form-validation/src/App.jsx
@@ -0,0 +1,232 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import axios from 'axios';
+
+function App() {
+  const [values, setvalues] = useState({
+    name: "",
+    email: "",
+    mobile: "",
+    zipcode: "",
+    Adress: "",
+    password: "",
+    counrty: "",
+    state: "",
+    city: "",
+  });
+
+  const handlechanges = (e) => {
+    setvalues({
+      ...values,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  async function handlefn(e) {
+    e.preventDefault()
+   
+    try {
+      const response = await axios.post("http://localhost:3000/post", values);
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+
+    console.log(values);
+  }
+
+  return (
+    <>
+      <div className="container">
+        <div className="contest-box">
+          <form onSubmit={handlefn}>
+            <div className="contestbox1">
+              <span id="names">Name</span>
+              <span className="ns">:</span>
+              <input
+                className="name"
+                minLength={10}
+                type="text"
+                placeholder="name"
+                name="name"
+                onChange={(e) => {
+                  handlechanges(e);
+                }}
+                required
+              />
+            </div>
+
+            <div className="contexbox2">
+              {" "}
+              <span id="emails">Email </span>
+              <span className="es">:</span>{" "}
+              <input
+                className="email"
+                type="email"
+                placeholder="email"
+                name="email"
+                onChange={(e) => {
+                  handlechanges(e);
+                }}
+                required
+              />
+            </div>
+
+            <div className="contexbox3">
+              {" "}
+              <span id="mobiles">Mobile</span>
+              <span className="ms">:</span>{" "}
+              <input
+                className="mobile"
+                type="tel"
+                minLength={10}
+                maxLength={10}
+                placeholder="Mobile"
+                name="mobile"
+                onChange={(e) => {
+                  handlechanges(e);
+                }}
+                required
+              />{" "}
+            </div>
+
+            <div className="contextbox4">
+              {" "}
+              <span id="zipcodes">Zipcode</span>
+              <span className="zs">:</span>{" "}
+              <input
+                className="zipcode"
+                type="text"
+                placeholder="Zipcode"
+                name="zipcode"
+                onChange={(e) => {
+                  handlechanges(e);
+                }}
+                required
+              />
+            </div>
+
+            <div className="contexbox5">
+              {" "}
+              <span id="address">Adress</span>
+              <span className="as">:</span>{" "}
+              <input
+                type="address"
+                className="add"
+                placeholder="Current-Adress"
+                name="Adress"
+                onChange={(e) => {
+                  handlechanges(e);
+                }}
+                required
+              />
+            </div>
+            <div className="contextbox6">
+              {" "}
+              <span id="passwords">password</span>
+              <span className="ps">:</span>{" "}
+              <input
+                className="pass"
+                maxLength={7}
+                type="password"
+                placeholder="password"
+                name="password"
+                onChange={(e) => {
+                  handlechanges(e);
+                }}
+                required
+              />
+            </div>
+
+            <div className="context-box7">
+              <span id="country">
+                Country<span className="countrys">:</span>
+              </span>
+              <select
+                id="countryname"
+                name="counrty"
+                onChange={(e) => {
+                  handlechanges(e);
+                }}
+                required
+              >
+                <option id="val" value="india">
+                  india
+                </option>
+                <option id="val" value="south-africa">
+                  South-africa
+                </option>
+                <option id="val" value="Australia">
+                  Australia
+                </option>
+                <option id="val" value="America">
+                  America
+                </option>
+              </select>
+            </div>
+
+            <span id="state">
+              State<span className="states">:</span>
+            </span>
+            <select
+              id="statename"
+              name="state"
+              onChange={(e) => {
+                handlechanges(e);
+              }}
+              required
+            >
+              <option id="val" value="madhya pradesh">
+                madhya pradesh
+              </option>
+              <option id="val" value="rajsthan">
+                rajsthan
+              </option>
+              <option id="val" value="punjab">
+                punjab
+              </option>
+              <option id="val" value="chandigrah">
+                chandigrah
+              </option>
+            </select>
+            <br />
+
+            <span id="city">
+              City <span className="citys">:</span>
+            </span>
+            <select
+              id="cityname"
+              name="city"
+              onChange={(e) => {
+                handlechanges(e);
+              }}
+              required
+            >
+              <option id="val" value="bhopal">
+                Bhopal
+              </option>
+              <option id="val" value="dewas">
+                Dewas
+              </option>
+              <option id="val" value="agra">
+                Agra
+              </option>
+              <option id="val" value="bakani">
+                Bakani
+              </option>
+            </select>
+
+            <br />
+
+            <input id="sd" type="submit" />
+          </form>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
+
